a41af5b019057c2409299737e7cea1228bc5ea9e
Gustavo I. Chalco <gchalcoa@gmail.com>
2021-02-05 20:19:16 -0500

    first commit

---
+++ src/PrimeraApp.tsx
@@ -0,0 +1,28 @@
+import { Fragment, ReactNode } from "react";
+import PropTypes from 'prop-types';
+
+interface IProps {
+  titulo?: ReactNode,
+  subtitulo?: ReactNode
+}
+
+// FC
+const PrimeraApp = ({ titulo, subtitulo } : IProps) => {
+
+  return (
+    <Fragment>
+      <h1>Titulo: { titulo }</h1>
+      <p>SubTitulo: { subtitulo }</p>
+    </Fragment>
+  );
+};
+
+PrimeraApp.propTypes = {
+  titulo: PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+  titulo: 'NONE'
+}
+
+export default PrimeraApp;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+
+import "./index.css";
+
+const divRoot = document.getElementById("root");
+
+ReactDOM.render(<PrimeraApp  subtitulo="Gustavo Chalco" />, divRoot);
+// 
